1fb853cb4b6af150ca096df358c491f5137ebeba
Ahmedshukr <ahmetoshukr@gmail.com>
2022-12-14 07:11:29 +0000

    data wrangling commit

---
+++ cellphone_data_wrangling.R
@@ -0,0 +1,13 @@
+install.packages("tidyverse")
+
+library(tidyverse)
+
+Cellphone <- read_csv("https://raw.githubusercontent.com/Ahmedshukr/Cell-Phone-Dataset-Data-Analysis/main/cell_phones_total.csv")
+
+View(Cellphone)
+
+View(select(Cellphone, "1980":"2020"))
+
+cellphone <- View(select(Cellphone, c('Country Name', '1980':'1991')))
+
+#assessing the dataset
