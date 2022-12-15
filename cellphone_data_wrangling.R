c3e07c11452d128772f90b612b969f2b0d874484
Ahmedshukr <ahmetoshukr@gmail.com>
2022-12-15 11:49:30 +0000

    updated

--- cellphone_data_wrangling.R
+++ cellphone_data_wrangling.R
@@ -1,3 +1,4 @@
+#installing the packages
 install.packages("tidyverse")
 
 library(tidyverse)
@@ -8,7 +9,36 @@ View(Cellphone)
 
 View(select(Cellphone, "1980":"2020"))
 
-View(select(Cellphone, c('Country Name', '1980':'1991')))
+View(select(Cellphone, c('Country Name', '1980':'1993')))
 
 #assessing the dataset
+str(Cellphone)
+
+Cellphone %>%  
+  select(is.numeric)
+
+Cellphone %>% 
+  select()
+#sum of the NA values
+sum(is.na(Cellphone))
+
+which(is.na(Cellphone))
+
+df <- replace(Cellphone, is.na(Cellphone), 0)
+sum(is.na(df))
+
+View(df)
+
+View(select(df, c('Country Name', '1980':'1992')))
+
+
+Cellphone[is.na(Cellphone)] <- 0
+
+select_if(df, is.numeric) 
+
+View(Cellphone)
+
+
+ggplot(Cellphone) +
+  geom_point(aes(x=2000, y = (Country Name)))
 
